f6df3320a1c22a3e167c3c92fdc7e7e291db284d
wahyu-atmojo <miripandrewgarfield@gmail.com>
2024-05-01 09:14:52 +0700

    model n table setoran sales

---
+++ app/Models/SalesSetoran.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SalesSetoran extends Model
+{
+    use HasFactory;
+}
